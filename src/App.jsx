ad7446ae37ca466e2f639931c30c6dd4d28a48aa
shivani283 <shivanivikhar283@gmail.com>
2023-10-12 19:24:10 +0530

    Values section added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import "./App.css"
 import Rescidencies from "./components/Residencies/Rescidencies";
+import Value from "./components/Value/Value";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Hero/>
       </div>
       <Rescidencies/>
+      <Value/>
     </div>
   );
 }
