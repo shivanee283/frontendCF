dadab8c877f6c383aecddd422f22e99ab3fb5b54
shivani283 <shivanivikhar283@gmail.com>
2023-10-13 16:59:56 +0530

    reverted changes

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Landing from "./Landing";
 import CreateCampaign from "./components/CreateCampaign/CreateCampaign";
 import Register from "./components/Register/Register";
 import CreateAccount from "./components/CreateAccount/CreateAccount";
+import Home from "./components/Home/Home";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/signup" element={<Register />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/" element={<Landing />} />
       </Routes>
       {/* <Landing /> */}
